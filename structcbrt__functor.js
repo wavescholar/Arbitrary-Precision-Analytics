6d50d34eb550b3497255045434628b8c1688868e
Bruce Campbell <wavescholar@gmail.com>
2014-04-23 13:59:10 -0400

    042314a

--- structcbrt__functor.js
+++ structcbrt__functor.js
@@ -1,6 +1,8 @@
 var structcbrt__functor =
 [
     [ "cbrt_functor", "structcbrt__functor.html#a141c3a705a6dc7a95c551ae9d374a21d", null ],
+    [ "cbrt_functor", "structcbrt__functor.html#a141c3a705a6dc7a95c551ae9d374a21d", null ],
+    [ "operator()", "structcbrt__functor.html#a30c7fc8100c8945c5326886f3e04d209", null ],
     [ "operator()", "structcbrt__functor.html#a30c7fc8100c8945c5326886f3e04d209", null ],
     [ "a", "structcbrt__functor.html#adb0eee75e5ce327d433a0264b51a3b0f", null ]
 ];
